ea76dfe8f1a1ae79a8c1d96ef6651d3417453745
myapp01 <thanawatpalaket9090@gmail.com>
2021-11-15 11:14:24 +0700

    initial commit

---
+++ bin/dartapp301.dart
@@ -0,0 +1,13 @@
+void main(){
+  //Variable Declatin and Variable Initialization
+  String msg = "Welcome to Dart"; //Local variable
+  String fullname = "Mark Sakaberg";
+  int age = 25;
+  int lastBirthDay = 2020;
+  int currentBirthDay = 2021;
+  int current = currentBirthDay - lastBirthDay;
+
+
+  //print(msg + fullname);
+  //print(age);
+}
